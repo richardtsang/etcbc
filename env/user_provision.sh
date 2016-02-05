3587535f362fb20f45f498f93f1e6a04a6cfe1a4
Richard Tsang <richard.tsang@etcbc.org>
2016-02-05 23:44:58 +0000

    Removing vagrant file and provisioning scripts

--- env/user_provision.sh
+++
@@ -1,7 +0,0 @@
-if [ ! -e ~/.bash-git-prompt ]
-then
-    git clone https://github.com/magicmonty/bash-git-prompt.git .bash-git-prompt
-    echo "source ~/.bash-git-prompt/gitprompt.sh" >> ~/.bashrc
-else
-    echo "Git Prompt already installed!"
-fi
